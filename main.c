aa52caf8ddce484f0333b2397bbd465693412dbc
Tron <itrustinshell@gmail.com>
2025-01-21 21:30:49 +0100

    history implementata

--- main.c
+++ main.c
@@ -2,14 +2,38 @@
 
 void prompt(char **inputstr)
 {
-	size_t len;
 	char *cwdpath;
+    char *cwdpath_modified;
+    int   len;
 
-	len = 0;
-	*inputstr = NULL;
 	cwdpath = getcwd(NULL, 0);
-	printf("%s💪💪💪: ", cwdpath);
-	getline(inputstr, &len, stdin); //free di inputstr fatto!
+	len = ft_strlen(cwdpath);
+
+    cwdpath_modified = malloc(len + 3);
+    strcpy(cwdpath_modified, cwdpath);
+    cwdpath_modified[len] = ' ';
+    cwdpath_modified[len + 1] = ':';
+    cwdpath_modified[len + 2] = '\0';
+
+    *inputstr = readline(cwdpath_modified);
+    if (*inputstr) 
+    {
+        size_t len = strlen(*inputstr);
+        char *with_newline = malloc(len + 2); // +1 per '\n' e +1 per '\0'
+        if (!with_newline) 
+            free(*inputstr);
+        strcpy(with_newline, *inputstr);
+        with_newline[len] = '\0'; //ho dovuto aggiungere qui un \0 al posto del \n precedentemente aggiuto. Pecckè a storie iev abbascie. Damn holy nothing.
+        with_newline[len + 1] = '\0'; //let's go to see (iamm a vrè)
+        free(*inputstr);
+        *inputstr = with_newline;
+    }
+    if (*inputstr)
+    {
+        add_history(*inputstr);
+    }
+    else
+        printf("weeee\n");
 }
 
 int main(int argc, char **argv, char **envp)
@@ -30,12 +54,16 @@ int main(int argc, char **argv, char **envp)
 	{	
 		inputstr = NULL;
 		prompt(&inputstr);
+      
+
 		token_matrix = tokenizer(inputstr); //TOKENIZER
+        
+
 		cmdlist = parsing(token_matrix); //PARSING
 		executor(cmdlist, &env, &exit_code); //EXECUTOR
 		//printf("sono nel main. Dopo executor. e questo è l'exit_code: %d\n", exit_code);
-		free(inputstr);
-		inputstr = NULL;
+		 free(inputstr);
+		// inputstr = NULL;
 		ft_freematrix(token_matrix);
 	}
 	return (0);
