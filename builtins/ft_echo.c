edbb1258a2a725be783dbbd852f59c02d827f6b6
Luigi Argenziano Vagello <largenzi@e3r9p7.42roma.it>
2025-02-01 15:33:50 +0100

    corretto comportamento di echo. Adesso echo senza alcun argomento funziona.

--- builtins/ft_echo.c
+++ builtins/ft_echo.c
@@ -4,12 +4,16 @@
 
 //TODO: gestire gli exit code...il momento in cui agirnare o stampare con echo, e il ritorno di tutti gli altri.
 
+/*
+	DESCRIPTION
+		simula il comportamento di echo.
+*/
 int	ft_echo(int argc, char **argv, int *exit_code)
 {
 	int	i;
 
 	(void)exit_code;
-	if (strcmp(argv[1], "-n") == 0)
+	if (argv[1] && strcmp(argv[1], "-n") == 0)
 	{
 		i = 2;
 		while (i < argc - 1)
@@ -21,6 +25,8 @@ int	ft_echo(int argc, char **argv, int *exit_code)
 	}
 	else
 	{
+		if (argc == 1)
+			return (1);
 		i = 1;
 		while (i < argc - 1)
 		{
