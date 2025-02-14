7aec04e2ab633a4a1b0ac33b69564db6d79be535
Luigi Argenziano Vagello <largenzi@e4r12p2.42roma.it>
2025-02-14 19:53:05 +0100

    fine

--- parser/expander.c
+++ parser/expander.c
@@ -3,19 +3,36 @@
 /*                                                        :::      ::::::::   */
 /*   expander.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dpalmese <dpalmese@student.42roma.it>      +#+  +:+       +#+        */
+/*   By: largenzi <largenzi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/13 12:23:36 by dpalmese          #+#    #+#             */
-/*   Updated: 2025/02/13 12:24:00 by dpalmese         ###   ########.fr       */
+/*   Updated: 2025/02/14 19:46:50 by largenzi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
-void	expand(char **env_var)
+void static	env(char **env_var)
 {
 	char	*expanded;
 
+	expanded = getenv((char *)*env_var);
+	if (!expanded)
+	{
+		free(*env_var);
+		*env_var = (char *)calloc(1, sizeof(char));
+		*env_var[0] = '\0';
+	}
+	else
+	{
+		free(*env_var);
+		*env_var = (char *)ft_calloc(ft_strlen(expanded) + 1, sizeof(char));
+		ft_strncpy(*env_var, expanded, ft_strlen(expanded));
+	}
+}
+
+void	expand(char **env_var)
+{
 	if (!env_var || !*env_var)
 		return ;
 	if (ft_strncmp(*env_var, "?", 1) == 0)
@@ -29,19 +46,5 @@ void	expand(char **env_var)
 		*env_var = ft_strdup("$");
 	}
 	else
-	{
-		expanded = getenv((char *)*env_var);
-		if (!expanded)
-		{
-			free(*env_var);
-			*env_var = (char *)calloc(1, sizeof(char));
-			*env_var[0] = '\0';
-		}
-		else
-		{
-			free(*env_var);
-			*env_var = (char *)ft_calloc(ft_strlen(expanded) + 1, sizeof(char));
-			ft_strncpy(*env_var, expanded, ft_strlen(expanded));
-		}
-	}
+		env(env_var);
 }
